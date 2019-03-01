39d4f56eb5407c2d41fde935e28d37d291863ee7
なるみ <weaper@gmail.com>
2019-03-01 17:27:19 +0800

    Modify main function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 from collections import OrderedDict
-from datetime import datetime, timedelta
+from datetime import datetime
 from time import sleep
 
 import tablib
@@ -11,30 +11,6 @@ from dateutil.rrule import MONTHLY, rrule
 from log import get_logger
 
 LOGGER = get_logger(__name__)
-
-SID0050 = [
-    '1101', '1102', '1216', '1301', '1303', '1326', '1402', '1722', '2002',
-    '2105', '2201', '2207', '2301', '2303', '2308', '2311', '2317', '2324',
-    '2325', '2330', '2347', '2353', '2354', '2357', '2382', '2409', '2412',
-    '2454', '2474', '2498', '2801', '2880', '2881', '2882', '2883', '2885',
-    '2886', '2890', '2891', '2892', '2912', '3008', '3045', '3231', '3481',
-    '3673', '3697', '4904', '5880', '6505'
-]
-
-SID0051 = [
-    '1227', '1304', '1314', '1319', '1434', '1440', '1504', '1507', '1590',
-    '1605', '1704', '1710', '1717', '1723', '1789', '1802', '1907', '2006',
-    '2015', '2049', '2101', '2103', '2106', '2204', '2327', '2337', '2344',
-    '2356', '2360', '2362', '2371', '2379', '2384', '2385', '2388', '2392',
-    '2393', '2395', '2448', '2450', '2451', '2501', '2504', '2511', '2542',
-    '2545', '2548', '2603', '2606', '2607', '2609', '2610', '2615', '2618',
-    '2707', '2723', '2727', '2809', '2812', '2823', '2834', '2845', '2855',
-    '2884', '2887', '2888', '2889', '2903', '2915', '3034', '3037', '3044',
-    '3149', '3189', '3406', '3474', '3702', '4725', '4938', '4958', '5522',
-    '5871', '6005', '6176', '6239', '6269', '6285', '6286', '8008', '8046',
-    '8078', '8422', '9904', '9907', '9914', '9917', '9921', '9933', '9940',
-    '9945'
-]
 
 
 def get_month_range_until_now(year, month):
@@ -90,13 +66,15 @@ class MonthIter(object):
 
 def parse_arg():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--stock-number', type=str, default='0050')
+    parser.add_argument('-s', '--stock', type=str, default='0050')
+    parser.add_argument('-o', '--output-dir', type=str, default='data')
     parser.add_argument(
         '-i',
         '--interval',
         type=int,
-        default=2,
+        default=5,
         help='Fetech interval in seconds')
+
     return parser.parse_args()
 
 
@@ -129,6 +107,8 @@ def fetch_history(sid, interval):
 
 
 def save_history(history, f):
+    os.makedirs(os.path.dirname(f), exist_ok=True)
+
     LOGGER.info(f'Saving history to {f}')
 
     # use tablib to save as csv file
@@ -147,42 +127,12 @@ def save_history(history, f):
 
 
 def main():
-    # args = parse_arg()
-    # sid = args.stock_number
-    # interval = args.interval
-    interval = 5
+    args = parse_arg()
 
-    for sid in SID0050:
-        f = f'sid0050/sid{sid}.csv'
+    history = fetch_history(args.stock, args.interval)
 
-        if os.path.exists(f):
-            LOGGER.info(f'{f} exists, skip')
-            continue
-
-        try:
-            history = fetch_history(sid, interval)
-        except Exception as e:
-            print(e)
-            continue
-
-        os.makedirs(os.path.dirname(f), exist_ok=True)
-        save_history(history, f)
-
-    for sid in SID0051:
-        f = f'sid0050/sid{sid}.csv'
-
-        if os.path.exists(f):
-            LOGGER.info(f'{f} exists, skip')
-            continue
-
-        try:
-            history = fetch_history(sid, interval)
-        except Exception as e:
-            print(e)
-            continue
-
-        history = fetch_history(sid, interval)
-        os.makedirs(os.path.dirname(f), exist_ok=True)
+    if history.values():
+        f = os.path.join(args.output_dir, '{}.csv'.format(args.stock))
         save_history(history, f)
 
 
